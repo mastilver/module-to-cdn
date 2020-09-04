b5268357d3866c200c42744a50cdc987e0f389d4
toutpt <toutpt@gmail.com>
2020-09-04 10:40:38 +0200

    test: rewrite cache system

--- test.js
+++ test.js
@@ -7,25 +7,39 @@ const fs = require('fs');
 const modules = require('./modules');
 const fn = require('.');
 
-const CACHE_NPM_PATH = '.npm-cache-info.json';
-const CACHE_NPM = {};
-if (fs.existsSync(CACHE_NPM_PATH)) {
-    Object.assign(CACHE_NPM, require(`./${CACHE_NPM_PATH}`));
-} else {
-    CACHE_NPM.__last = 0;
+const CACHE_BASE_PATH = '.test-cache';
+if (!fs.existsSync(CACHE_BASE_PATH)) {
+    console.log(`Setup cache for testing`);
+    fs.mkdirSync(CACHE_BASE_PATH);
 }
 
-const AXIOS_CACHE_BASE_PATH = '.axios-cache';
-const AXIOS_CACHE_PATH = `${AXIOS_CACHE_BASE_PATH}/index.json`;
-const AXIOS_CACHE = {};
-if (fs.existsSync(AXIOS_CACHE_PATH)) {
-    Object.assign(AXIOS_CACHE, require(`./${AXIOS_CACHE_PATH}`));
-} else {
-    if (!fs.existsSync(AXIOS_CACHE_BASE_PATH)) {
-        fs.mkdirSync(AXIOS_CACHE_BASE_PATH);
-    }
+const CACHE_NPM_PATH = `${CACHE_BASE_PATH}/.npm-cache-info.json`;
+const CACHE_NPM = {
+    __last: Date.now(),
+};
 
-    fs.writeFileSync(AXIOS_CACHE_PATH, '{}');
+if (fs.existsSync(CACHE_NPM_PATH)) {
+    const cache = require(`./${CACHE_NPM_PATH}`);
+    if (Date.now() - cache.__last < 1000 * 60 * 60) {
+        console.log('use cache on NPM');
+        Object.assign(CACHE_NPM, cache);
+    } else {
+        console.log('reset NPM cache');
+    }
+}
+
+const AXIOS_CACHE_INDEX_PATH = `${CACHE_BASE_PATH}/axios.json`;
+const AXIOS_CACHE_PATH = `${CACHE_BASE_PATH}/axios`;
+const AXIOS_CACHE = {};
+if (fs.existsSync(AXIOS_CACHE_INDEX_PATH)) {
+    Object.assign(AXIOS_CACHE, require(`./${AXIOS_CACHE_INDEX_PATH}`));
+} else {
+    console.log(`Setup cache index for axios request in ${AXIOS_CACHE_INDEX_PATH}`);
+    fs.writeFileSync(AXIOS_CACHE_INDEX_PATH, '{}');
+}
+if (!fs.existsSync(AXIOS_CACHE_PATH)) {
+    console.log(`Setup cache folder for axios request in ${AXIOS_CACHE_PATH}`);
+    fs.mkdirSync(AXIOS_CACHE_PATH)
 }
 
 const moduleNames = Object.keys(modules);
@@ -37,9 +51,9 @@ function cachedGet(url) {
     }
 
     return axios.get(url).then(response => {
-        AXIOS_CACHE[url] = `./.axios-cache/cache-${Math.random().toFixed(10)}.js`;
+        AXIOS_CACHE[url] = `./${AXIOS_CACHE_PATH}/cache-${Math.random().toFixed(10)}.js`;
         fs.writeFileSync(AXIOS_CACHE[url], response.data);
-        fs.writeFileSync(AXIOS_CACHE_PATH, JSON.stringify(AXIOS_CACHE, null, 2));
+        fs.writeFileSync(AXIOS_CACHE_INDEX_PATH, JSON.stringify(AXIOS_CACHE, null, 2));
         return response;
     });
 }
@@ -87,11 +101,6 @@ for (const moduleName of moduleNames.filter(m => limit(m))) {
         test.serial(`prod: ${moduleName}@${version}`, testModule, moduleName, version, 'production');
         test.serial(`dev: ${moduleName}@${version}`, testModule, moduleName, version, 'development');
     });
-}
-
-if (Date.now() - CACHE_NPM.__last > 1000 * 60 * 60) {
-    CACHE_NPM.__last = Date.now();
-    fs.writeFileSync(CACHE_NPM_PATH, JSON.stringify(CACHE_NPM));
 }
 
 async function testModule(t, moduleName, version, env) {
@@ -159,6 +168,7 @@ function getModuleInfo(moduleName) {
             'dist-tags': info['dist-tags'],
             versions: info.versions
         };
+        fs.writeFileSync(CACHE_NPM_PATH, JSON.stringify(CACHE_NPM));
     }
 
     return CACHE_NPM[moduleName];
